aad2f27d1421f5262f8685989758a2224d299eb6
wuqinduo <wuqinduo@example.com>
2016-11-03 19:23:10 +0800

    add sso

---
+++ taotao-sso/src/main/java/com/taotao/sso/dao/JedisClient.java
@@ -0,0 +1,5 @@
+package com.taotao.sso.dao;
+
+public interface JedisClient {
+
+}
---
+++ taotao-sso/src/main/java/com/taotao/sso/service/UserRegisterService.java
@@ -0,0 +1,11 @@
+package com.taotao.sso.service;
+
+/**
+ * 用户注册服务
+ * @author Administrator
+ *
+ */
+public interface UserRegisterService {
+
+	
+}
